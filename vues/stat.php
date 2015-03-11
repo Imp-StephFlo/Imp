843d392d91fb0244c38860faa0a1f500c409d3e5
Takiguchi <takiguchi@TakiPackarbian>
2015-03-11 19:49:58 +0100

    Correction du bug lors du clic sur ''Valider''

--- vues/stat.php
+++ vues/stat.php
@@ -3,173 +3,162 @@
 include('../util/dbconnect.php');
 //On inclut la liste des fonctions
 include('../util/fonctions.php');
-//On récupére et on vérifie
-include('../util/verification.php');
+if(isset($_POST['Periode'])) //Si la variables periode exite et code n'ai pas vide on peut faire le graphique
+{
+    echo "je suis passé par là !";
+    $periode=$_POST['Periode']; //On la récupére
+    
+    //recherche de la somme d'impression selon la période choisi ainsi que la date du jour
+    $total=nbImpressions($code,$periode);
+    
+    //On calcul les statistiques selon la période choisi et le code utilisateur
+    if($periode=='mois')
+    {
+        //On recherche son nombre maximun d'impression et a quel mois 
+        //Création de 2 variables qui contiendra les résultats de la requete 
+        $periodeT = array();
+        $somme = array();
 
-    if(isset($_POST['Periode']) and $Code!="") //Si la variables periode exite et code n'ai pas vide on peut faire le graphique
-         {
-            $periode=$_POST['Periode']; //On la récupére
-            
-            //recherche de la somme d'impression selon la période choisi ainsi que la date du jour
-            $total=nbImpressions($Code,$periode);
-            
-            //On calcul les statistiques selon la période choisi et le code utilisateur
-            if($periode=='mois')
+        //Fonction pour récupérer le résultat moyen selon la periode voulu
+        $resultSomme=StatPerso($code,$periode);
+
+        //Rangement des résultats dans les tableaux
+        $i=0;
+        $max=0;
+        $min=100000;
+        while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM)) 
+        {
+            $periodeT[$i] = $row[0];
+            $somme[$i] = floor($row[1]);
+            if($somme[$i]>$max)
             {
-                //On recherche son nombre maximun d'impression et a quel mois 
-                //Création de 2 variables qui contiendra les résultats de la requete 
-                $periodeT = array();
-                $somme = array();
-
-                //Fonction pour récupérer le résultat moyen selon la periode voulu
-                $resultSomme=StatPerso($Code,$periode);
-
-                //Rangement des résultats dans les tableaux
-                $i=0;
-                $max=0;
-                $min=100000;
-                while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM)) 
-                    {
-                        $periodeT[$i] = $row[0];
-                        $somme[$i] = floor($row[1]);
-                        if($somme[$i]>$max)
-                        {
-                            $max=$somme[$i];
-                            $dateMax=$periodeT[$i];
-                        }
-                        
-                        if($somme[$i]<$min)
-                        {
-                            $min=$somme[$i];
-                            $dateMin=$periodeT[$i];
-                        }
-                        $i++;
-                    }
-                //Calcul des coûts 
-                    //cout en euros
-                        //En noir recto/Verso
-                        $cout=($max/10)*0.15;
-                        $coutMax=number_format($cout,2);
-                    //cout en arbres
-                        $ArbMax=$max/15000;//15 000 feuilles donne 1 arbres
-                        $coutArbMax=number_format($ArbMax,2);
-                    
-                    //cout en euros
-                        //En noir recto/Verso
-                        $coutMin=($min/10)*0.15;
-                        $coutMini=number_format($coutMin,2);
-                    //cout en arbres
-                        $ArbMin=$min/15000;//15 000 feuilles donne 1 arbres
-                        $coutArbMin=number_format($ArbMin,2);
+                $max=$somme[$i];
+                $dateMax=$periodeT[$i];
+            } 
+            if($somme[$i]<$min)
+            {
+                $min=$somme[$i];
+                $dateMin=$periodeT[$i];
             }
-            elseif ($periode=="annee") 
-                {
-                    //On recherche son nombre maximun d'impression et a quel année 
-                    //Création de 2 variables qui contiendra les résultats de la requete 
-                    $periodeT = array();
-                    $somme = array();
+            $i++;
+        }//fin while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM)) 
+        //Calcul des coûts 
+            //cout en euros
+            //En noir recto/Verso
+        $cout=($max/10)*0.15;
+        $coutMax=number_format($cout,2);
+            //cout en arbres
+        $ArbMax=$max/15000;//15 000 feuilles donne 1 arbres
+        $coutArbMax=number_format($ArbMax,2);
+        
+            //cout en euros
+            //En noir recto/Verso
+        $coutMin=($min/10)*0.15;
+        $coutMini=number_format($coutMin,2);
+        //cout en arbres
+        $ArbMin=$min/15000;//15 000 feuilles donne 1 arbres
+        $coutArbMin=number_format($ArbMin,2);
+    }//fin if($periode=='mois')
+    elseif ($periode=="annee") 
+    {
+        //On recherche son nombre maximun d'impression et a quel année 
+        //Création de 2 variables qui contiendra les résultats de la requete 
+        $periodeT = array();
+        $somme = array();
 
-                    //Fonction pour récupérer le résultat moyen selon la periode voulu
-                    $resultSomme=StatPerso($Code,$periode);
+        //Fonction pour récupérer le résultat moyen selon la periode voulu
+        $resultSomme=StatPerso($code,$periode);
 
-                    //Rangement des résultats dans les tableaux
-                    $i=0;
-                    $max=0;
-                    $min=1000000;
-                    while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM)) 
-                        {
-                            $periodeT[$i] = $row[0];
-                            $somme[$i] = floor($row[1]);
-                            if($somme[$i]>$max)
-                            {
-                                $max=$somme[$i];
-                                $dateMax=$periodeT[$i];
-                            }
+        //Rangement des résultats dans les tableaux
+        $i=0;
+        $max=0;
+        $min=1000000;
+        while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM)) 
+        {
+            $periodeT[$i] = $row[0];
+            $somme[$i] = floor($row[1]);
+            if($somme[$i]>$max)
+            {
+                $max=$somme[$i];
+                $dateMax=$periodeT[$i];
+            }//fin if($somme[$i]>$max)
+            if($somme[$i]<$min)
+            {
+                $min=$somme[$i];
+                $dateMin=$periodeT[$i];
+            }//fin if($somme[$i]<$min)
+            $i++;
+        }//fin while
+        //Calcul des coûts 
+            //cout en euros
+            //En noir recto/Verso
+        $cout=($max/10)*0.15;
+        $coutMax=number_format($cout,2);
+            //cout en arbres
+        $ArbMax=$max/15000;//15 000 feuilles donne 1 arbres
+        $coutArbMax=number_format($ArbMax,2);
 
-                            if($somme[$i]<$min)
-                            {
-                                $min=$somme[$i];
-                                $dateMin=$periodeT[$i];
-                            }
-                            $i++;
-                        }
-                    //Calcul des coûts 
-                        //cout en euros
-                            //En noir recto/Verso
-                            $cout=($max/10)*0.15;
-                            $coutMax=number_format($cout,2);
-                        //cout en arbres
-                            $ArbMax=$max/15000;//15 000 feuilles donne 1 arbres
-                            $coutArbMax=number_format($ArbMax,2);
+            //cout en euros
+            //En noir recto/Verso
+        $coutMin=($min/10)*0.15;
+        $coutMini=number_format($coutMin,2);
+            //cout en arbres
+        $ArbMin=$min/15000;//15 000 feuilles donne 1 arbres
+        $coutArbMin=number_format($ArbMin,2);
+    }//fin elseif ($periode=="annee")
+    else
+    {
+        //On recherche son nombre maximun d'impression et a quel jour 
+        //Création de 2 variables qui contiendra les résultats de la requete 
+        $periodeT = array();
+        $somme = array();
 
-                        //cout en euros
-                            //En noir recto/Verso
-                            $coutMin=($min/10)*0.15;
-                            $coutMini=number_format($coutMin,2);
-                        //cout en arbres
-                            $ArbMin=$min/15000;//15 000 feuilles donne 1 arbres
-                            $coutArbMin=number_format($ArbMin,2);
-                
-                }
-                else
-                {
-                    //On recherche son nombre maximun d'impression et a quel jour 
-                    //Création de 2 variables qui contiendra les résultats de la requete 
-                    $periodeT = array();
-                    $somme = array();
+        //Fonction pour récupérer le résultat moyen selon la periode voulu
+        $resultSomme=StatPerso($code,$periode);
 
-                    //Fonction pour récupérer le résultat moyen selon la periode voulu
-                    $resultSomme=StatPerso($Code,$periode);
-
-                    //Rangement des résultats dans les tableaux
-                    $i=0;
-                    $max=0;
-                    $min=10000;
-                    while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM)) 
-                        {
-                            $periodeT[$i] = $row[0];
-                            $somme[$i] = floor($row[1]);
-                            if($somme[$i]>$max)
-                            {
-                                $max=$somme[$i];
-                                $dateMax=$periodeT[$i];
-                            }
-
-                            if($somme[$i]<$min)
-                            {
-                                $min=$somme[$i];
-                                $dateMin=$periodeT[$i];
-                            }
-                            $i++;
-                        }
-                    //Calcul des coûts 
-                        //cout en euros
-                            //En noir recto/Verso
-                            $cout=($max/10)*0.15;
-                            $coutMax=number_format($cout,2);
-                        //cout en arbres
-                            $ArbMax=$max/15000;//15 000 feuilles donne 1 arbres
-                            $coutArbMax=number_format($ArbMax,2);
-
-                        //cout en euros
-                            //En noir recto/Verso
-                            $coutMin=($min/10)*0.15;
-                            $coutMini=number_format($coutMin,2);
-                        //cout en arbres
-                            $ArbMin=$min/15000;//15 000 feuilles donne 1 arbres
-                            $coutArbMin=number_format($ArbMin,2);
-                }
-            
-         }
-            
+        //Rangement des résultats dans les tableaux
+        $i=0;
+        $max=0;
+        $min=10000;
+        while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM)) 
+        {
+            $periodeT[$i] = $row[0];
+            $somme[$i] = floor($row[1]);
+            if($somme[$i]>$max)
+            {
+                $max=$somme[$i];
+                $dateMax=$periodeT[$i];
+            }
+            if($somme[$i]<$min)
+            {
+                $min=$somme[$i];
+                $dateMin=$periodeT[$i];
+            }
+            $i++;
+        }//fin while ($row = mysql_fetch_array($resultSomme, MYSQL_NUM))
+        //Calcul des coûts 
+            //cout en euros
+            //En noir recto/Verso
+        $cout=($max/10)*0.15;
+        $coutMax=number_format($cout,2);
+            //cout en arbres
+        $ArbMax=$max/15000;//15 000 feuilles donne 1 arbres
+        $coutArbMax=number_format($ArbMax,2);
+            //cout en euros
+            //En noir recto/Verso
+        $coutMin=($min/10)*0.15;
+        $coutMini=number_format($coutMin,2);
+            //cout en arbres
+        $ArbMin=$min/15000;//15 000 feuilles donne 1 arbres
+        $coutArbMin=number_format($ArbMin,2);
+    }//fin else
+}//fin if(isset($_POST['Periode']) and $code!="")
 ?>
 
 <!--Contenue du corps du tableau-->
     <tr>
     <!--Debut du formulaire-->
-        <?php
-        echo "<form action='index.php?action=stat".$code."' method='POST'>";
-        ?>
+      
       <td rowspan="5">
         <img src="./images/index_17.jpg" width="33" height="463" alt="" />
       </td>
@@ -188,14 +177,15 @@ include('../util/verification.php');
                       <div class="h1">Statistiques</div>
                       <br />
                       <div class="content_statL">
+                          <form action='./index.php?action=stat<?php echo $code;?>' method='POST'>
                           <input type='radio' id='pj' name='Periode' value='jour'/><label for='pj'> Jour</label> &nbsp;
                           <input type='radio' id='pm' name='Periode' value='mois' checked/><label for='pm'> Mois</label> &nbsp;
                           <input type='radio' id='pa' name='Periode' value='annee'/><label for='pa'> Ann&eacute;e</label> &nbsp;
                           <input type="submit" id="valider" value="Valider"/><br /><br />
-                
+                          </form>
                             <?php
-                                    if(isset($max) and $periode=="mois")
-                                        {
+    if(isset($max) and $periode=="mois")
+    {
                             ?>            
                             <p>
                                 Maximum : <?php echo $max?> pages imprim&eacute;es, au mois de <?php echo $dateMax?> .<br />
@@ -207,11 +197,11 @@ include('../util/verification.php');
                                 <!--Ce mois-ci, vous en étes à <?php echo $total?> pages imprim&eacute;es.<br />-->
                             </p>
             
-                            <?php
-                                        }
-                                        elseif (isset($max) and $periode=="annee") 
-                                            {
-                            ?> 
+<?php
+    }//fin if(isset($max) and $periode=="mois")
+    elseif (isset($max) and $periode=="annee") 
+    {
+?> 
                             <p>
                                 Maximum : <?php echo $max?> pages imprim&eacute;es, dans l'ann&eacute;e <?php echo $dateMax?> .<br />
                                 <span class="marge">Co&ucirc;tent: <?php echo $coutMax?> euros.</span><br />
