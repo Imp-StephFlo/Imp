473f1f4b3a2860795e5aff50d40632be7237c11b
Takiguchi <takiguchi@TakiDebian>
2015-03-17 14:31:04 +0100

    Ajout du fichier ajax pour l'onglet 'Global'

---
+++ ajax/genererGraphiqueMoyenne.php
@@ -0,0 +1,30 @@
+<?php
+include('../util/dbconnect.php');
+require_once('../util/fonctions.php');
+includeAllRequiredFiles();
+getConnection();
+//Récupération des variables
+$code       = $_REQUEST['code'];
+$periode    = $_REQUEST['periode'];
+//Création de 2 variables qui contiendra les résultats de la requete 
+$periodeT = array();
+$moyenne = array();
+//Fonction pour récupérer le résultat moyen selon la periode voulu
+$resultMoyenne=StatMoyen($periode);
+//Rangement des résultats dans les tableaux
+$i=0;
+while ($row = mysql_fetch_array($resultMoyenne, MYSQL_NUM)) 
+{
+    $periodeT[$i] = $row[0];
+    $moyenne[$i] = floor($row[1]/44);
+    $i++;
+}//fin while
+// /!\ Le nom du graphique sera le code de l'utilisateur /!\
+//Appel de la fonction pour créer le graphique
+
+//===> graphiqueCourbe($periodeT, $moyenne, 'Periode', 'Moyenne', 'NomDuGraphique');
+graphiqueCourbe($periodeT,$moyenne,'Periode','Moyenne',substr($code,6), $periode);
+//Retourne le code de l'utilisateur, qui est également le nom du graphique (le fichier image)
+//echo '<img src="./images/'.$code.'.png" id="graphique" name="graphique" width="462" height="200" alt="" contenteditable="false" />'
+
+echo '<img src="./images/'.substr($code,6).$periode.'.png" id="graphique" name="graphique" width="462" height="200" alt="" contenteditable="false" />';
